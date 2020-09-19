92a14460463f82f8c5aa444a6bc7fa72af1d5df5
Aayush Rajasekaran <arajasek94@gmail.com>
2020-09-18 23:46:03 -0400

    Introduce v1 actors

---
+++ chain/actors/builtin/account/v1.go
@@ -0,0 +1,16 @@
+package account
+
+import (
+	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/lotus/chain/actors/adt"
+	"github.com/filecoin-project/specs-actors/v2/actors/builtin/account"
+)
+
+type state1 struct {
+	account.State
+	store adt.Store
+}
+
+func (s *state1) PubkeyAddress() (address.Address, error) {
+	return s.Address, nil
+}
